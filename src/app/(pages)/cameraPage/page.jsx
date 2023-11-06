aa475076587aa791487689f57ddb3053b94f1c83
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-11-06 20:47:59 -0300

    Adicionando tela de dados da bicicleta

--- src/app/(pages)/cameraPage/page.jsx
+++ src/app/(pages)/cameraPage/page.jsx
@@ -1,27 +1,27 @@
-export default function CaperaPage() {
-  return (
-    <div className='camera-page'>
-      <h1 className='titulo-camera'>Detecção de Objetos</h1>
-      <button
-        id="startButton"
-        onClick={handleStartDetection}
-        disabled={isDetectionStarted}
-        style={{ display: isDetectionStarted ? 'none' : 'block' }}
-      >
-        Iniciar Detecção
-      </button>
-      <button
-        id="stopButton"
-        onClick={handleStopDetection}
-        disabled={!isDetectionStarted}
-        style={{ display: isDetectionStarted ? 'block' : 'none' }}
-      >
-        Parar Detecção
-      </button>
-      <div className="webcam">
-        <img ref={videoRef} src="http://localhost:8080/video_feed" alt="Detecção de objetos" className='deteccao'/>
-        <canvas ref={detectionsCanvasRef}></canvas>
-      </div> 
-    </div>
-  )
-}
+// export default function CaperaPage() {
+//   return (
+//     <div className='camera-page'>
+//       <h1 className='titulo-camera'>Detecção de Objetos</h1>
+//       <button
+//         id="startButton"
+//         onClick={"handleStartDetection"}
+//         disabled={"isDetectionStarted"}
+//         style={{ display: isDetectionStarted ? 'none' : 'block' }}
+//       >
+//         Iniciar Detecção
+//       </button>
+//       <button
+//         id="stopButton"
+//         onClick={handleStopDetection}
+//         disabled={!isDetectionStarted}
+//         style={{ display: isDetectionStarted ? 'block' : 'none' }}
+//       >
+//         Parar Detecção
+//       </button>
+//       <div className="webcam">
+//         <img ref={videoRef} src="http://localhost:8080/video_feed" alt="Detecção de objetos" className='deteccao'/>
+//         <canvas ref={detectionsCanvasRef}></canvas>
+//       </div> 
+//     </div>
+//   )
+// }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,9 +19,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className="reset">
-        <Cabecalho />
+        
           {children}
-        <Rodape />
+        
       </body>
     </html>
   );
